e858e40999fef2144d9cdccbebc78edb75a8da8f
TaB199 <bankky_tj@yahoo.com>
2022-12-08 15:18:37 -0800

    Task 0 to 6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - It prints the string with a newline and returns an integer value
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
