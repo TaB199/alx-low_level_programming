31547e184b7ce281601a2ea67a70ed74c6681054
TaB199 <bankky_tj@yahoo.com>
2022-12-15 16:11:17 -0800

    Tasks 0 to 3

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int main(void);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif
